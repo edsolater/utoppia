26b03212be43a48f084f9375d67ca49fc2f4540d
edsolater <edsolater@qq.com>
2023-01-04 01:30:07 +0800

    🚧 use scrollDetector

--- src/components/uikitExamples/components/DevExample.tsx
+++ src/components/uikitExamples/components/DevExample.tsx
@@ -1,17 +1,36 @@
 import { Div } from '@edsolater/uikit'
-import { scrollDetecter, scrollDetecterCSSVariableNames } from '@edsolater/uikit/plugins'
+import { useDOM } from '@edsolater/uikit/hooks'
+import { scrollDetecter } from '@edsolater/uikit/plugins'
 import { ExamplePanel } from '../utils/ExamplePanel'
 
 export function DevExample() {
+  const [bar, setBar] = useDOM()
   return (
     <ExamplePanel name='Dev'>
-      <Div
-        icss={{ height: `calc(300px + var(${scrollDetecter.cssVariable['--dy']}, 0) * 1px)`, overflow: 'auto' }}
-        plugin={scrollDetecter}
-      >
-        <Div icss={{ width: 200, height: 200, background: 'linear-gradient(dodgerblue, skyblue)' }}></Div>
-        <Div icss={{ width: 200, height: 200, background: 'linear-gradient(dodgerblue, skyblue)' }}></Div>
-        <Div icss={{ width: 200, height: 200, background: 'linear-gradient(dodgerblue, skyblue)' }}></Div>
+      <Div icss={{ position: 'relative' }}>
+        <Div
+          domRef={setBar}
+          icss={{
+            height: 50,
+            background: '#bc0d0dd9',
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            transform: `translateY(clamp(-100%, var(${scrollDetecter.cssVariable['--speed-y']}) * 1000px, 0px))`,
+            transition: '300ms'
+          }}
+        ></Div>
+
+        <Div
+          icss={{
+            height: 300,
+            // overflow: 'auto'
+          }}
+          plugin={scrollDetecter({ detectTargetDOM: bar })}
+        >
+          <Div icss={{ width: 200, height: 200, background: 'linear-gradient(dodgerblue, skyblue)' }}></Div>
+        </Div>
       </Div>
     </ExamplePanel>
   )
