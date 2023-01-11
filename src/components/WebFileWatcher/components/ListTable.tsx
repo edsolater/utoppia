5dfd81ea58bf790db922cead66df644cd56b955e
zhanggongying <edsolater@qq.com>
2023-01-11 19:18:37 +0800

    🚧

--- src/components/WebFileWatcher/components/ListTable.tsx
+++ src/components/WebFileWatcher/components/ListTable.tsx
@@ -1,16 +1,18 @@
 import { isObject } from '@edsolater/fnkit'
-import { AddProps, createKit, Div, DivChildNode, DivProps, For, Group, ICSSObject } from '@edsolater/uikit'
+import { AddProps, createKit, Div, DivChildNode, DivProps, For, Group, ICSSObject, Row } from '@edsolater/uikit'
 
 export type ListTableProps<T extends Record<string, any> = Record<string, any>> = {
   items: T[]
   getItemKey?: (info: { item: T; idx: number }) => string | number
 
   // -------- sub --------
-  renderItem?: (info: { item: T }) => DivChildNode
-  renderHeader?: (info: { items: T[]; firstItem?: T }) => DivChildNode
+  // renderItem?: (info: { item: T }) => DivChildNode
+  // renderHeader?: (info: { items: T[]; firstItem?: T }) => DivChildNode
+
   anatomy?: {
-    item?: DivProps
-    header?: DivProps
+    itemCell?: DivProps
+    itemRow?: DivProps
+    headerCell?: DivProps
     itemGroup?: DivProps
     headerGroup?: DivProps
   }
@@ -20,25 +22,28 @@ export const ListTable = createKit(
   'ListTable',
   <T extends Record<string, any>>({ items, getItemKey, anatomy }: ListTableProps<T>) => {
     const itemPropertyNames = getItemsProperties(items)
-    console.log('itemPropertyNames: ', itemPropertyNames)
     const gridICSS: ICSSObject = { display: 'grid', gap: 24, gridTemplateColumns: `1fr 2fr 48px` }
     return (
-      <Div icss={{ border: '1px solid', padding: 4 }}>
+      <Div icss={items.length ? { border: '1px solid', padding: 4 } : { display: 'none' }}>
+        {/* header */}
         <Group shadowProps={anatomy?.headerGroup} name='list-header'>
-          <Div shadowProps={anatomy?.header}>
+          <Row shadowProps={anatomy?.headerCell}>
             <For each={itemPropertyNames}>
               {(n) => <Div icss={{ marginBlock: 4, fontSize: 18, fontWeight: 'bold' }}>{n}</Div>}
             </For>
-          </Div>
+          </Row>
         </Group>
 
+        {/* list content */}
         <Group shadowProps={anatomy?.itemGroup} name='list-item-group' icss={{ display: 'grid', gap: 8 }}>
           <For each={items} getKey={(item, idx) => getItemKey?.({ item, idx })}>
             {(item) => (
-              <AddProps shadowProps={anatomy?.item}>
-                <Div icss={gridICSS}>
+              <AddProps shadowProps={anatomy?.itemCell}>
+                <Div icss={gridICSS} shadowProps={anatomy?.itemRow}>
                   <For each={Object.values(item)} getKey={(_, idx) => idx}>
-                    {(itemValue) => <Div>{isObject(itemValue) ? '[todo]' : itemValue}</Div>}
+                    {(itemValue) => (
+                      <Div shadowProps={anatomy?.itemCell}>{isObject(itemValue) ? '[todo]' : itemValue}</Div>
+                    )}
                   </For>
                 </Div>
               </AddProps>
