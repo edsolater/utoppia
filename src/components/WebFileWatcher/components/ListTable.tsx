b97818e5258417b2aa2048a84295f2cf10784faa
edsolater <edsolater@qq.com>
2023-01-12 01:02:12 +0800

    🚧 useTableCellWidthDetector

--- src/components/WebFileWatcher/components/ListTable.tsx
+++ src/components/WebFileWatcher/components/ListTable.tsx
@@ -1,5 +1,6 @@
 import { isObject } from '@edsolater/fnkit'
 import { AddProps, createKit, Div, DivChildNode, DivProps, For, Group, ICSSObject, Row } from '@edsolater/uikit'
+import { RefObject } from 'react'
 
 export type ListTableProps<T extends Record<string, any> = Record<string, any>> = {
   items: T[]
@@ -23,29 +24,46 @@ export const ListTable = createKit(
   <T extends Record<string, any>>({ items, getItemKey, anatomy }: ListTableProps<T>) => {
     const itemPropertyNames = getItemsProperties(items)
     const gridICSS: ICSSObject = { display: 'grid', gap: 24, gridTemplateColumns: `1fr 2fr 48px` }
+    const { tableRef, createTabelCellRef, hasDetected, getCellWidth } = useTableCellWidthDetector()
     return (
-      <Div icss={items.length ? { border: '1px solid', padding: 4 } : { display: 'none' }}>
+      <Div
+        domRef={tableRef}
+        icss={items.length && hasDetected ? { border: '1px solid', padding: 4 } : { display: 'none' }}
+      >
         {/* header */}
         <Group shadowProps={anatomy?.headerGroup} name='list-header'>
           <Row shadowProps={anatomy?.headerCell}>
             <For each={itemPropertyNames}>
-              {(n) => <Div icss={{ marginBlock: 4, fontSize: 18, fontWeight: 'bold' }}>{n}</Div>}
+              {(n, idx) => (
+                <Div
+                  domRef={createTabelCellRef(idx)}
+                  icss={{ marginBlock: 4, fontSize: 18, fontWeight: 'bold', width: getCellWidth(idx) }}
+                >
+                  {n}
+                </Div>
+              )}
             </For>
           </Row>
         </Group>
 
         {/* list content */}
-        <Group shadowProps={anatomy?.itemGroup} name='list-item-group' icss={{ display: 'grid', gap: 8 }}>
+        <Group name='list-item-group' shadowProps={anatomy?.itemGroup}>
           <For each={items} getKey={(item, idx) => getItemKey?.({ item, idx })}>
             {(item) => (
               <AddProps shadowProps={anatomy?.itemCell}>
-                <Div icss={gridICSS} shadowProps={anatomy?.itemRow}>
-                  <For each={Object.values(item)} getKey={(_, idx) => idx}>
-                    {(itemValue) => (
-                      <Div shadowProps={anatomy?.itemCell}>{isObject(itemValue) ? '[todo]' : itemValue}</Div>
+                <Row shadowProps={anatomy?.itemRow} icss={{ marginBlock: 8, background: 'crimson' }}>
+                  <For each={Object.values(item)}>
+                    {(itemValue, idx) => (
+                      <Div
+                        icss={{ width: getCellWidth(idx) }}
+                        domRef={createTabelCellRef(idx)}
+                        shadowProps={anatomy?.itemCell}
+                      >
+                        {isObject(itemValue) ? '[todo]' : itemValue}
+                      </Div>
                     )}
                   </For>
-                </Div>
+                </Row>
               </AddProps>
             )}
           </For>
@@ -54,6 +72,20 @@ export const ListTable = createKit(
     )
   }
 )
+
+/**
+ * @example
+ * getItemsProperties([{a:1, b:2}, {a:2, c:2}, {a:3, b:3, d:3}]) //=> ['a', 'b', 'c', 'd']
+ */
 function getItemsProperties(items: Record<string, any>[]) {
   return [...new Set(items.flatMap((i) => Object.keys(i)))]
+}
+
+function useTableCellWidthDetector(): {
+  tableRef: RefObject<HTMLElement>
+  createTabelCellRef: (colIndex: number) => RefObject<HTMLElement>
+  hasDetected: boolean
+  getCellWidth(colIndex: number): number | undefined // px
+} {
+  throw new Error('Function not implemented.')
 }
